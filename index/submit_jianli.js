53d10044bdcc741a8bb51ca8ceb4c060f4b2887a
husterL9 <2670227240@qq.com>
2021-07-04 16:35:12 +0800

    7月4日

---
+++ index/submit_jianli.js
@@ -0,0 +1,12 @@
+function tijiao(){  
+      var x;
+      var r=confirm("你确定提交吗？");
+      if (r==true){
+        alert("提交成功") 
+        window.location.href='homepage.html'
+      }
+      else{
+      }
+      document.getElementById("demo").innerHTML=x;
+   } 
+   
